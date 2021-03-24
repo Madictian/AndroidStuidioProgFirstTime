b71b3c29ae5de91d08814da438a941a85ee25fb5
Madictian <fredrikbille@hotmail.com>
2021-03-24 22:04:42 +0100

    Snackbar Added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,19 +1,26 @@
 package com.example.myapplication;
 
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.view.View;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText givenNumberOne;
-    private EditText givenNumberTwo;
-    private TextView showResult;
-    private Button calculate;
+    private EditText omsætning;
+    private EditText vareforbrug;
+    private EditText bruttofortjeneste;
+    private EditText markedsfoeringsomkostninger;
+    private EditText markedsfoeringsbidrag;
+    private EditText oevrigeKapacitetsomkostninger;
+    private EditText indtjeningsbidrag;
+    private EditText afskrivninger;
+    private EditText resultatFoerRenter;
+    private EditText renteomkostninger;
+    private TextView result;
+    private Button knap;
 
 
 
@@ -23,27 +30,113 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        givenNumberOne = (EditText) findViewById(R.id.givenNumberOne);
-        givenNumberOne.setText("");
+        omsætning = findViewById(R.id.omsætning);
+        final String OsPlaceholder = "omsætning";
+        omsætning.setHint(OsPlaceholder);
 
-        givenNumberTwo = (EditText) findViewById(R.id.givenNumberTwo);
-        givenNumberTwo.setText("");
+        vareforbrug = findViewById(R.id.vareforbrug);
+        final String VfPlaceholder = "vareforbrug";
+        vareforbrug.setHint(VfPlaceholder);
 
-        showResult = (TextView) findViewById(R.id.showResult);
+        bruttofortjeneste = findViewById(R.id.bruttofortjeneste);
+        final String BfTPlaceholder = "bruttofortjeneste";
+        bruttofortjeneste.setHint(BfTPlaceholder);
 
-        calculate = findViewById(R.id.calculate);
-        calculate.setOnClickListener(new View.OnClickListener() {
+        markedsfoeringsomkostninger = findViewById(R.id.markedsfoeringsomkostninger);
+        final String MfOPlaceholder = "markedsfoeringsomkostninger";
+        markedsfoeringsomkostninger.setHint(MfOPlaceholder);
+
+        markedsfoeringsbidrag = findViewById(R.id.markedsfoeringsbidrag);
+        final String MfBPlaceholder = "markedsfoeringsbidrag";
+        markedsfoeringsbidrag.setHint(MfBPlaceholder);
+
+        oevrigeKapacitetsomkostninger = findViewById(R.id.oevrigeKapacitetsomkostninger);
+        final String ovKoPlaceholder = "oevrigeKapacitetsomkostninger";
+        oevrigeKapacitetsomkostninger.setHint(ovKoPlaceholder);
+
+        indtjeningsbidrag = findViewById(R.id.indtjeningsbidrag);
+        final String IbPlaceholder = "indtjeningsbidrag";
+        indtjeningsbidrag.setHint(IbPlaceholder);
+
+        afskrivninger = findViewById(R.id.afskrivninger);
+        final String AsPlaceholder = "afskrivninger";
+        afskrivninger.setHint(AsPlaceholder);
+
+        resultatFoerRenter = findViewById(R.id.resultatFoerRenter);
+        final String RfRPlaceholder = "resultatFoerRenter";
+        resultatFoerRenter.setHint(RfRPlaceholder);
+
+        renteomkostninger = findViewById(R.id.renteomkostninger);
+        final String RomPlaceholder = "renteomkostninger";
+        renteomkostninger.setHint(RomPlaceholder);
+
+        result = findViewById(R.id.result);
+        final String resultText = "result = ";
+        result.setText(resultText);
+
+        knap = findViewById(R.id.Udregn);
+        knap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                double first = Double.parseDouble(givenNumberOne.getText().toString());
-                double second = Double.parseDouble(givenNumberTwo.getText().toString());
-                double result = first * second;
-                showResult.setText(String.valueOf(result));
+                double first = 0.0,second = 0.0,third = 0.0,fourth = 0.0,fifth = 0.0,sixth = 0.0,seventh = 0.0,eighth = 0.0,ninth = 0.0,tenth = 0.0, Fresult = 0.0;
 
-                Snackbar.make(v, "Resultatet er " + result + "!", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                if(omsætning.getText().toString().isEmpty()){omsætning.setText(String.valueOf(first));}else{first = Double.parseDouble(omsætning.getText().toString());}
+                if(vareforbrug.getText().toString().isEmpty()){vareforbrug.setText(String.valueOf(second));}else{second = Double.parseDouble(vareforbrug.getText().toString());}
+                if(markedsfoeringsomkostninger.getText().toString().isEmpty()){markedsfoeringsomkostninger.setText(String.valueOf(fourth));}else{fourth = Double.parseDouble(markedsfoeringsomkostninger.getText().toString());}
+                if(oevrigeKapacitetsomkostninger.getText().toString().isEmpty()){oevrigeKapacitetsomkostninger.setText(String.valueOf(sixth));}else{sixth = Double.parseDouble(oevrigeKapacitetsomkostninger.getText().toString());}
+                if(afskrivninger.getText().toString().isEmpty()){afskrivninger.setText(String.valueOf(eighth));}else{eighth = Double.parseDouble(afskrivninger.getText().toString());}
+                if(renteomkostninger.getText().toString().isEmpty()){renteomkostninger.setText(String.valueOf(tenth));}else{tenth = Double.parseDouble(renteomkostninger.getText().toString());}
+                third = first - second;
+
+                fifth = third - fourth;
+
+                seventh = fifth - sixth;
+
+                ninth = seventh - eighth;
+
+                Fresult = ninth - tenth;
+                result.setText(resultText + Fresult);
+
+                omsætning = findViewById(R.id.omsætning);
+                omsætning.setText(String.valueOf(first));
+
+                vareforbrug = findViewById(R.id.vareforbrug);
+                vareforbrug.setText(String.valueOf(second));
+
+                bruttofortjeneste = findViewById(R.id.bruttofortjeneste);
+                bruttofortjeneste.setText(String.valueOf(third));
+
+                markedsfoeringsomkostninger = findViewById(R.id.markedsfoeringsomkostninger);
+                markedsfoeringsomkostninger.setText(String.valueOf(fourth));
+
+                markedsfoeringsbidrag = findViewById(R.id.markedsfoeringsbidrag);
+                markedsfoeringsbidrag.setText(String.valueOf(fifth));
+
+                oevrigeKapacitetsomkostninger = findViewById(R.id.oevrigeKapacitetsomkostninger);
+                oevrigeKapacitetsomkostninger.setText(String.valueOf(sixth));
+
+                indtjeningsbidrag = findViewById(R.id.indtjeningsbidrag);
+                indtjeningsbidrag.setText(String.valueOf(seventh));
+
+                afskrivninger = findViewById(R.id.afskrivninger);
+                afskrivninger.setText(String.valueOf(eighth));
+
+                resultatFoerRenter = findViewById(R.id.resultatFoerRenter);
+                resultatFoerRenter.setText(String.valueOf(ninth));
+
+                renteomkostninger = findViewById(R.id.renteomkostninger);
+                renteomkostninger.setText(String.valueOf(tenth));
+
+
+
+
+
+
+
             }
         });
+
+
 
 
     }
